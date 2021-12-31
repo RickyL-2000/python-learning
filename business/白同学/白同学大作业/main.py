bfa2df9f0e4ac7d3569450b511e847142bdb148a
RickyL-2000 <ruiqili4@illinois.edu>
2021-12-31 16:09:12 +0800

    白同学 done

--- business/白同学/白同学大作业/main.py
+++ business/白同学/白同学大作业/main.py
@@ -1,179 +1,176 @@
 import threading
-import time
+import os
 import random
 
-"""
-Ideas:
-1. 把Cat()本身做成timer
-"""
+# 所有可能的状态
+all_status = {
+    "walk": "我在散步......",
+    "play": "我在玩耍......",
+    "feed": "我在吃饭......",
+    "seedoctor": "我在看医生......",
+    "sleep": "我在睡觉...",
+    "idle": "我醒着但我很无聊......"
+}
 
-"""
-Problems:
-1. 当猫醒来的时候，需要自动触发输出一条信息表示它醒了吗？
-"""
+if os.path.exists("cat.csv"):
+    # 从文件中回复状态
+    with open("cat.csv", "r") as f:
+        data = f.read().split()
+        cat_happiness = int(data[0])
+        cat_hunger = int(data[1])
+        cat_health = int(data[2])
+        cat_status = data[3]
+        cat_time = int(data[4])
+else:
+    # 随机初始化
+    cat_happiness = random.randint(0, 100)
+    cat_hunger = random.randint(0, 100)
+    cat_health = random.randint(0, 100)
+    cat_time = random.randint(0, 23)
+    if cat_time < 8:
+        cat_status = "sleep"
+    else:
+        cat_status = "idle"
 
-# constants
-TICK_TACK = 5
-WAKE_UP = 8
-FALL_ASLEEP = 24
-EXIT = -2
+# 让所有值都在0和100之间
+def check_value(value):
+    return min(100, max(0, value))
 
-Commands = ["walk", "play", "feed", "seedoctor", "letalone", "status", "bye"]
-States = ["walk", "play", "feed", "seedoctor", "letalone", "sleep", "waiting_for_disturb"]
+# 修改自func_timer函数
+def clock():        
+    global cat_time
+    global cat_status
+    global timer
 
-class CatTommy():
-    def __init__(self):
-        # timer
-        self.clock = 8
-        self.t_begin = time.time()
-        self.t_end = time.time()
-        # states
-        self.state = "letalone"
-        # value
-        self.hunger_value = int(random.randint(1, 100))
-        self.happy_value = int(random.randint(1, 100))
-        self.health_value = int(random.randint(1, 100))
+    global cat_happiness
+    global cat_hunger
+    global cat_health
+
+    #8点醒0点睡。多print一次使得界面看上去更加友好
+    if cat_time == 0:
+        cat_status = "sleep"
+        print(f"\n\n{all_status[cat_status]}\n\n你想: ", end="")
+    elif cat_time == 8:
+        cat_status = "idle"
+        print(f"\n\n{all_status[cat_status]}\n\n你想: ", end="")
+
+    # 基础指标更新
+    if cat_status == "idle":
+        cat_hunger += 2
+        cat_happiness -= 1
+    elif cat_status =="sleep":
+        cat_hunger += 1
+    elif cat_status =="walk":
+        cat_hunger += 3
+        cat_health += 1
+    elif cat_status =="play":
+        cat_hunger += 3
+        cat_happiness += 1
+    elif cat_status =="feed":
+        cat_hunger -= 3
+    elif cat_status =="seedoctor":
+        cat_health += 4
+
+    if cat_hunger > 80 or cat_hunger < 20:
+        cat_health -= 2
+
+    if cat_happiness < 20:
+        cat_health -= 2
+
+    cat_hunger = check_value(cat_hunger)
+    cat_health = check_value(cat_health)
+    cat_happiness = check_value(cat_happiness)
+
+    # 时间从0到23循环
+    cat_time = cat_time + 1
+    if cat_time > 23:
+        cat_time = 0
     
-    def run(self):
-        print("Initial message")
-        self.update_clock()
-        self.print_state()
-        while True:
-            command = input("你想: ")
-            res = self.process_input(command)
-            if res == -1 or res == 0:
-                continue
-            elif res == EXIT:
-                # exit the program
-                self.save_checkpoints()
-                print("记得来找我！Bye...")
-                break
-            else:
-                self.update_state(res)
-    
-    def update_state(self, new_state, verbose=True):
-        if self.state == new_state:
-            return
-        if self.state == "sleep" and new_state in ["walk", "play", "feed", "seedoctor"]:
-            self.state = "waiting_for_disturb"
-            self.handle_disturb_waiting(new_state)
-            return
-        if self.state == "sleep" and new_state == "letalone":
-            if verbose: self.print_state()
-            return
-        if new_state in ["walk", "play", "feed", "seedoctor", "letalone"]:
-            self.state = new_state
-            if verbose: self.print_state()
-        elif new_state == "sleep":
-            self.state = new_state  # no printing
-        else:
-            print(f"Something went wrong. Illegal state name: {new_state}")
+    timer = threading.Timer(5.0, clock)
+    timer.start()
 
-    def handle_disturb_waiting(self, new_state):
-        confirm = input("你确认要吵醒我吗？我在睡觉，你要是坚持吵醒我，我会不高兴的！(y表示是/其他表示不是):")
-        if confirm != "y":
-            self.state = "sleep"
-            return
-        if self.state == "sleep":
-            self.state = new_state
-            self.happy_value -= 4
-            self.__value_check()
-            self.print_state(cur_time=False, cur_values=False)
-        else:
-            # NOTE: 如果它睡着了，你打扰它，但是你过了很久，等它醒了再坚持带它去活动，则不会扣happy值
-            self.state = new_state
-            self.print_state()
+# 处理命令，转移各个状态
+def command_cat(command):
+    global cat_status
+    if command == "walk":
+        cat_status = "walk"
+        print(all_status["walk"] + "\n")
+    elif command == "play":
+        cat_status = "play"
+        print(all_status["play"] + "\n")
+    elif command == "feed":
+        cat_status = "feed"
+        print(all_status["feed"] + "\n")
+    elif command == "seedoctor":
+        cat_status = "seedoctor"
+        print(all_status["seedoctor"] + "\n")
+    elif command == "letalone":
+        # 假如此时是醒着的
+        cat_status = "idle"
+        print(all_status["idle"] + "\n")
+    else:
+        print("我不懂你在说什么\n")
 
-    def update_clock(self):
-        self.t_end = time.time()
-        time_passed = int(self.t_end - self.t_begin) // TICK_TACK
-        self.clock = (self._clock + int(self.t_end - self.t_begin) // TICK_TACK) % 24
-        self.t_begin = time.time()
-        return time_passed
+# 进度条
+def progress_bar(value):
+    return "*"*(value//2) + "-"*(50-value//2)
 
-    def print_state(self, cur_time=True, cur_state=True, cur_values=True):
-        if cur_time:
-            print(f"当前时间: {self.clock}点")
-        if cur_state:
-            print("我当前的状态:", end="")
-            if self.state == "walk":
-                print("我在散步.....")
-            elif self.state == "play":
-                print("我在玩耍.....")
-            elif self.state == "feed":
-                print("我在吃饭.....")
-            elif self.state == "seedoctor":
-                print("我在看医生.....")
-            elif self.state == "letalone":
-                print("我醒着但很无聊.....")
-            elif self.state == "sleep":
-                print("我在睡觉.....")
-        if cur_values:
-            print(f"Happiness:  Sad {'*'*int(self.happy_value/2) + '_'*int(50-self.happy_value//2)} Happy({self.happy_value:03})")
-            print(f"Hungry:    Full {'*'*int(self.happy_value/2) + '_'*int(50-self.happy_value//2)} Hungry({self.happy_value:03})")
-            print(f"Health:    Sick {'*'*int(self.happy_value/2) + '_'*int(50-self.happy_value//2)} Healthy({self.happy_value:03})")
-        
-    def process_input(self, command):
-        if command not in Commands:
-            print("我不懂你在说什么")
-            return -1
-        if command in ["walk", "play", "feed", "seedoctor", "letalone"]:
-            return command
-        if command == "status":
-            time_passed = self.update_clock()
-            self.update_values(time_passed)
-            self.print_state()
-            return 0
-        if command == "bye":
-            # close the program
-            return EXIT
+def command_status():
+    print(f"\n当前时间: {cat_time}点\n我当前的状态: {all_status[cat_status]}")
 
-    def update_values(self, time_passed):
-        for t in range(time_passed):
-            if self.state == "letalone":
-                self.hunger_value += 2
-                self.happy_value -= 1
-            elif self.state == "sleep" or self.state == "waiting_for_disturb":
-                self.hunger_value += 1
-            elif self.state == "walk":
-                self.hunger_value += 3
-                self.health_value += 1
-            elif self.state == "play":
-                self.hunger_value += 3
-                self.health_value += 1
-            elif self.state == "feed":
-                self.hunger_value -= 3
-            elif self.state == "seedoctor":
-                self.health_value += 4
-
-            # value check
-            self.__value_check()
-
-            # normalize
-            self.hunger_value = max(0, min(100, self.hunger_value))
-            self.happy_value = max(0, min(100, self.happy_value))
-            self.health_value = max(0, min(100, self.health_value))
-    
-    def __value_check(self):
-        if self.hunger_value > 80 or self.hunger_value < 20:
-            self.health_value -= 2
-        if self.happy_value < 20:
-            self.health_value -= 1
-
-    def load_checkpoints(self):
-        pass
-
-    def save_checkpoints(self):
-        pass
+    print("Happiness:   Sad " + progress_bar(cat_happiness) + f" Happy({cat_happiness:03})")
+    print("Hungry:     Full " + progress_bar(cat_hunger) + f" Hungry({cat_hunger:03})")
+    print("Health:     Sick " + progress_bar(cat_health) + f" Healthy({cat_health:03})")
+    print()
 
 if __name__ == '__main__':
-    
-    t_begin = time.time()
+    print("我的名字叫Tommy，一只可爱的猫咪....")
+    print("你可以和我一起散步玩耍，你也可以给我好吃的东西，带我去看病，也可以让我发呆....")
+    print("Commands:")
+    print("1.walk:散步")
+    print("2.play:玩耍")
+    print("3.feed:喂我")
+    print("4.see doctor:看医生")
+    print("5.letalone:让我独自一猫")
+    print("6.status:查看我的状态")
+    print("7.bye:不想看到我\n")
+
+    command_status()
+    clock()
 
     while True:
-        key = input("please input anything:")
-        t_end = time.time()
-        print(f"time has passed {t_end - t_begin}")
-        t_begin = time.time()
-        if key == "q":
+        command = input("你想: ")
+        if command == "status":
+            command_status()
+        elif command == "bye":
+            with open("cat.csv", "w") as f:
+                f.write(str(cat_happiness) + "\n")
+                f.write(str(cat_hunger) + "\n")
+                f.write(str(cat_health) + "\n")
+                f.write(str(cat_status) + "\n")
+                f.write(str(cat_time) + "\n")
+            print("记得来找我!Bye...")
+            timer.cancel()
             break
+        else:
+            if cat_time < 8:
+                # 此时tommy在睡觉
+                if command == "letalone":
+                    # 此时，无论如何tommy都应该睡回去
+                    cat_status = "sleep"
+                    print(all_status["sleep"] + "\n")
+                elif cat_status != "sleep":
+                    # 如果没有睡(已经被叫醒)那就继续做事
+                    command_cat(command)
+                else:
+                    # 需要先吵醒
+                    msg = input("你确定要吵醒我吗？我在睡觉，你要是坚持吵醒我，我会不高兴的！（y表示是/其他表示不是）")
+                    if msg == "y":
+                        cat_happiness -= 4
+                        cat_happiness = check_value(cat_happiness)
+                        command_cat(command)
+                    else:
+                        print()
+            else:
+                command_cat(command)
+
